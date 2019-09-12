937a5411702c8fa47079dab4ccf34f02e9e12da0
Varthan <gv@loketa.com>
2019-09-12 11:57:10 +0530

    TC_EOS_Tracker

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./TC_Eos_Tracker'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/TC_Eos_Tracker/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 3000, () => { console.log("TC EOS Tracker runs: http://10.0.2.15:3000")});
